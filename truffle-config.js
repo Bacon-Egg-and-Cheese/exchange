7f0349b4d1acd95057562e9d5f12a87d5fc51b34
msolomonTMG <msolomon@thrillist.com>
2021-07-17 12:37:04 -0400

    created erc-20 token

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
